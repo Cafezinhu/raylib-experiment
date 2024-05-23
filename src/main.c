c1ded88491f7f2096ab56a3b20f6e2a931d48f6a
Aerton <aertoncastro@gmail.com>
2024-05-23 14:20:41 -0300

    web export

--- src/main.c
+++ src/main.c
@@ -1,16 +1,32 @@
 #include "raylib.h"
 
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
+const int screenWidth = 800;
+const int screenHeight = 600;
+
+void UpdateDrawFrame();
+
 int main() {
-  const int screenWidth = 800;
-  const int screenHeight = 600;
+
   InitWindow(screenWidth, screenHeight, "Raylib basic window");
+#if defined(PLATFORM_WEB)
+  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
   // SetTargetFPS(60);
   while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("It works!", 20, 100, 20, BLACK);
-    EndDrawing();
+    UpdateDrawFrame();
   }
+#endif
   CloseWindow();
   return 0;
+}
+
+void UpdateDrawFrame() {
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
+  DrawText("It works!", 20, 100, 20, BLACK);
+  EndDrawing();
 }

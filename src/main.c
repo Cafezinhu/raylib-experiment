a56e8c9b76f4a10313b111a95c25e5e0926ab892
Aerton <aertoncastro@gmail.com>
2024-05-23 19:11:13 -0300

    load texture

--- src/main.c
+++ src/main.c
@@ -4,14 +4,27 @@
 #include <emscripten/emscripten.h>
 #endif
 
+void UpdateDrawFrame();
+
 const int screenWidth = 800;
 const int screenHeight = 600;
 
-void UpdateDrawFrame();
+//------------------ASSETS--------------
+Texture tex;
 
 int main() {
 
   InitWindow(screenWidth, screenHeight, "Raylib basic window");
+
+#ifndef PLATFORM_ANDROID
+  ChangeDirectory("assets");
+#endif
+
+  tex = LoadTexture("flat-dark.png");
+
+#ifndef PLATFORM_ANDROID
+  ChangeDirectory("..");
+#endif
 #if defined(PLATFORM_WEB)
   emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
@@ -28,5 +41,8 @@ void UpdateDrawFrame() {
   BeginDrawing();
   ClearBackground(RAYWHITE);
   DrawText("It works!", 20, 100, 20, BLACK);
+  const Rectangle rec = {0, 160, 160, 160};
+  const Vector2 pos = {0, 0};
+  DrawTextureRec(tex, rec, pos, WHITE);
   EndDrawing();
 }
